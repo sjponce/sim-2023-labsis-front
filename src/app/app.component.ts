4e234e7ceb1db6134e6385fea08cdb59e394aea5
sjponce <santiago.j.ponce@gmail.com>
2023-05-07 23:10:56 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 import {
   BehaviorSubject,
   combineLatest,
-  debounceTime,
-  distinctUntilChanged,
   map,
   shareReplay,
+  skip,
   startWith,
   switchMap,
   tap,
@@ -47,6 +46,7 @@ export class AppComponent {
     'inicioTrabajoT2Meta',
     'tiempoTrabajoT2Meta',
     'finTrabajoT2Meta',
+    'promedioPermanenciaEquiposMeta',
   ];
   protected displayedColumns = [
     'n',
@@ -68,10 +68,14 @@ export class AppComponent {
     'inicioTrabajoT2',
     'tiempoTrabajoT2',
     'finTrabajoT2',
+    'promedioPermanenciaEquipos'
   ];
 
   protected configForm = this.fb.group({
-    n: [1000, [Validators.required, Validators.min(0), Validators.pattern("^[0-9]*$")]],
+    tiempoCierre: [
+      1440,
+      [Validators.required, Validators.min(0), Validators.pattern('^[0-9]*$')],
+    ],
     cotaInfLlegada: [30, [Validators.required, Validators.min(0)]],
     cotaSupLlegada: [90, [Validators.required, Validators.min(0)]],
     probA: [0.3, [Validators.required, Validators.min(0), Validators.max(1)]],
@@ -88,13 +92,18 @@ export class AppComponent {
     cotaSupVariacionTrabajo: [5, [Validators.required]],
     tiempoTrabajoInicialC: [25, [Validators.required, Validators.min(0)]],
     tiempoTrabajoFinalC: [10, [Validators.required, Validators.min(0)]],
-    tamanoCola: [3, [Validators.required, Validators.min(0), Validators.pattern("^[0-9]*$")]]
+    tamanoCola: [
+      3,
+      [Validators.required, Validators.min(0), Validators.pattern('^[0-9]*$')],
+    ],
   });
 
   protected pageNumber = 0;
+  protected isLoading$ = new BehaviorSubject(false);
+  protected stats: any;
 
   protected paginator$ = new BehaviorSubject<Paginator>({
-    limit: 50,
+    limit: 400,
     skip: 0,
   });
   protected readonly reload$ = new BehaviorSubject<null>(null);
@@ -102,42 +111,44 @@ export class AppComponent {
     this.paginator$,
     this.reload$,
   ]).pipe(
+    //skip(1),
     switchMap(([paginator]) =>
       this.rowService.getAll(paginator.limit, paginator.skip)
     ),
-    tap(() => (this.totalCount = this.configForm.controls.n.value ?? 0)),
     shareReplay({ bufferSize: 1, refCount: true }),
     startWith([])
   );
 
   protected readonly bounds$ = this.rows$.pipe(
     map((rows) => {
-      let min = Infinity;
+      let min = 0;
       let max = 0;
       
       rows.forEach((row: any) => {
         if (row.trabajos) {
           Object.keys(row.trabajos).forEach((trabajo: any) => {
-            min =
-            row.trabajos[trabajo].id < min ? row.trabajos[trabajo].id : min;
             max =
-            row.trabajos[trabajo].id > max ? row.trabajos[trabajo].id : max;
+            row.trabajos[trabajo].id >= max ? row.trabajos[trabajo].id : max;
           });
-        };
+        }
       });
-
+      
+      console.log(max);
       return [min, max];
     }),
-    shareReplay({bufferSize: 1, refCount: true}),
+    shareReplay({ bufferSize: 1, refCount: true })
   );
 
   protected readonly jobHeaders$ = this.bounds$.pipe(
     map(([min, max]) => {
       const res: string[] = [];
-      for (let i = min; i < max; i++) {
+      for (let i = min; i <= max; i++) {
         res.push(`T${i}Estado`);
         res.push(`T${i}Llegada`);
         res.push(`T${i}InicioTrabajo`);
+        res.push(`T${i}InicioTrabajoSolitario`);
+        res.push(`T${i}FinTrabajoSolitario`);
+        res.push(`T${i}InicioTrabajoFinal`);
         res.push(`T${i}FinTrabajo`);
       }
       return res;
@@ -148,10 +159,13 @@ export class AppComponent {
   protected readonly jobMetaHeaders$ = this.bounds$.pipe(
     map(([min, max]) => {
       const res: string[] = [];
-      for (let i = min; i < max; i++) {
+      for (let i = min; i <= max; i++) {
         res.push(`T${i}EstadoMeta`);
         res.push(`T${i}LlegadaMeta`);
         res.push(`T${i}InicioTrabajoMeta`);
+        res.push(`T${i}InicioTrabajoSolitarioMeta`);
+        res.push(`T${i}FinTrabajoSolitarioMeta`);
+        res.push(`T${i}InicioTrabajoFinalMeta`);
         res.push(`T${i}FinTrabajoMeta`);
       }
       return res;
@@ -168,12 +182,19 @@ export class AppComponent {
   );
 
   public generate() {
-    this.rowService.generate(this.configForm.getRawValue()).subscribe();
+    this.isLoading$.next(true);
+    this.rowService
+      .generate(this.configForm.getRawValue())
+      .subscribe((stats: any) => {
+        this.stats = stats;
+        this.isLoading$.next(false);
+        this.totalCount = stats.n;
+      });
   }
 
   public populate() {
     this.paginator$.next({
-      limit: 50,
+      limit: 400,
       skip: 0,
     });
   }
@@ -189,8 +210,8 @@ export class AppComponent {
     const value = this.paginator$.value;
     this.paginator$.next({
       limit: value.limit,
-      skip: value.limit * this.pageNumber
-    })
+      skip: value.limit * this.pageNumber,
+    });
   }
 
   public getHeader(header: string) {
@@ -198,6 +219,12 @@ export class AppComponent {
       return 'Estado';
     } else if (header.includes('Llegada')) {
       return 'Llegada';
+    } else if (header.includes('InicioTrabajoSolitario')) {
+      return 'Inicio trabajo solitario';
+    } else if (header.includes('FinTrabajoSolitario')) {
+      return 'Fin trabajo solitario';
+    } else if (header.includes('InicioTrabajoFinal')) {
+      return 'Inicio trabajo Final';
     } else if (header.includes('InicioTrabajo')) {
       return 'Inicio trabajo';
     } else {
@@ -205,7 +232,7 @@ export class AppComponent {
     }
   }
 
-  public getMetaHeader(header: string, index: number) {
+  public getMetaHeader(header: string) {
     if (header.includes('EstadoMeta')) {
       return `TRABAJO ${header.match(/\d+/)?.shift()}`;
     } else {
@@ -215,13 +242,18 @@ export class AppComponent {
 
   public getRowValue(header: string, row: any) {
     let index = `T${header.match(/\d+/)?.shift()}`;
-    if( !row?.trabajos || !row.trabajos[index]) return '';
+    if (!row?.trabajos || !row.trabajos[index]) return '';
 
     if (header.includes('Estado')) {
       return row.trabajos[index].estado;
     } else if (header.includes('Llegada')) {
-      
       return row.trabajos[index].llegada?.toFixed(4);
+    } else if (header.includes('InicioTrabajoSolitario')) {
+      return row.trabajos[index].tiempoFinTrabajoInicialC?.toFixed(4);
+    } else if (header.includes('FinTrabajoSolitario')) {
+      return row.trabajos[index].tiempoFinTrabajoSolitarioC?.toFixed(4);
+    } else if (header.includes('InicioTrabajoFinal')) {
+      return row.trabajos[index].tiempoInicioTrabajoFinalC?.toFixed(4);
     } else if (header.includes('InicioTrabajo')) {
       return row.trabajos[index].inicioTrabajo?.toFixed(4);
     } else {
